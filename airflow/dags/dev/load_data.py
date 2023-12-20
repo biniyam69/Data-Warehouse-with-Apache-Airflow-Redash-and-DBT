379e94dd11173220e29d12206566451f0679f764
biniyam <biniyam.ajaw@gmail.com>
2023-12-20 23:00:24 +0300

    created a dockerized env

---
+++ airflow/dags/dev/load_data.py
@@ -0,0 +1,50 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime
+import pandas as pd
+import psycopg2
+
+
+def load_csv_to_pg():
+    try:
+        conn = psycopg2.connect(
+            dbname='your_db_name',
+            user='your_db_user',
+            password='your_db_password',
+            host='your_db_host',
+            port='your_db_port'
+        )
+
+        csv_path = '/home/biniyam/dwh-project-UAV/data/week2data.csv'
+
+        # Load CSV into Pandas DataFrame
+        df = pd.read_csv(csv_path)
+
+        # Assuming table name is 'your_table_name' and you want to append data
+        df.to_sql('your_table_name', conn, if_exists='append', index=False)
+
+        conn.commit()
+        conn.close()
+        print("CSV data loaded into PostgreSQL successfully!")
+    except Exception as e:
+        print("Error:", e)
+
+
+# Define Airflow DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 12, 1),
+    'retries': 1,
+}
+
+dag = DAG('load_csv_to_pg_dag',
+          default_args=default_args,
+          schedule_interval='@daily')
+
+# Define the PythonOperator
+load_data_task = PythonOperator(
+    task_id='load_data_task',
+    python_callable=load_csv_to_pg,
+    dag=dag,
+)
